6a6654235122239630173a051c918724420f94d2
Yash-Sarode <84972646+Yash-Sarode@users.noreply.github.com>
2021-06-06 21:15:40 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Web Development with Javascript and React.js</h1>
+      <p>
+        Html Javascript and React.js online bootcamp conducted by ShapeAI.
+      </p>
+    </div>
+  );
+}
+export default Note;
